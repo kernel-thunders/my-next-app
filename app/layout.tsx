67955db3d35f457e91bed28b8308b1153c1c2962
226137 <zx4453714@gmail.com>
2024-12-13 11:13:31 +0900

    変更

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 // app/layout.tsx
 import "./globals.css";
-import Sidebar from "../components/Sidebar";
-import Header from "@/components/Header";
-import Profile from "@/components/Profile";
-import Buttons from "@/components/Buttons";
+import Sidebar from "./components/Sidebar";
+import Header from "@/app/components/Header";
+import Profile from "@/app/components/Profile";
+import Buttons from "@/app/components/Buttons";
 
 export const metadata = {
   title: "Home",
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body>{children}
-      <main >
+      <main>
       <Sidebar />
       </main>
       </body>
